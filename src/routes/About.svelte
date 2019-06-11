410428e11ef9cc051893fba9d93df52b85133eb0
Manki Kim <cpro95@gmail.com>
2019-06-11 22:46:43 +0900

    version 1.0 finished

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -3,12 +3,10 @@
 </script>
 
 <style>
-  .herostyle {
-    opacity: 0.95;
-    background: #499bea;
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr=#499bea, endColorstr=#798eea, GradientType=1);
-  }
 </style>
 
 <Header />
-<p>This SPA app is about to search my movie within my KODI DB</p>
+<div class="container-fluid">
+  <p>This SPA app is about to search my movie within my KODI DB</p>
+  <p>Made by SvelteJS, Bootstrap CSS</p>
+</div>
